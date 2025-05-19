17112e5ded17355adafa952d97029dde44dbf19e
RimAaloi <aaloirim@example.com>
2025-05-19 11:26:21 +0200

    update

--- aaloirim/src/main/java/aaloirim/aaloirim/repository/ClientRepository.java
+++ aaloirim/src/main/java/aaloirim/aaloirim/repository/ClientRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    // Rechercher un client par email (utile pour vérifier si un client existe déjà)
     Optional<Client> findByEmail(String email);
 }
--- aaloirim/src/main/java/aaloirim/aaloirim/repository/CreditRepository.java
+++ aaloirim/src/main/java/aaloirim/aaloirim/repository/CreditRepository.java
@@ -7,12 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CreditRepository extends JpaRepository<Credit, Long> {
-    // Rechercher les crédits d'un client spécifique
     List<Credit> findByClientId(Long clientId);
 
-    // Rechercher les crédits par statut
+
     List<Credit> findByStatut(Statut statut);
 
-    // Rechercher les crédits d'un client avec un statut spécifique
     List<Credit> findByClientIdAndStatut(Long clientId, Statut statut);
 }
